1dcb1b80ff28ea283be3485e3b2d505d70d053dd
Josh <saxman1819@aol.com>
2023-11-11 20:53:09 -0500

    Adding these files to the repo.  They're a work in progress.

---
+++ SimpleTlsRedirector.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import socket
+import threading
+import select
+import sys
+import ssl
+
+terminateAll = False
+
+class ClientThread(threading.Thread):
+	def __init__(self, clientSocket, targetHost, targetPort, cert):
+		threading.Thread.__init__(self)
+		self.__clientSocket = clientSocket
+		self.__targetHost = targetHost
+		self.__targetPort = targetPort
+		self.__cert = cert
+		
+	def run(self):
+		print("Client Thread started")
+		
+		self.__clientSocket.setblocking(0)
+
+		context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+		context.load_verify_locations(self.__cert)
+		
+		targetHostSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		targetHostSocket = context.wrap_socket(targetHostSocket, server_hostname='Blackhawk')
+		targetHostSocket.connect((self.__targetHost, self.__targetPort))
+		targetHostSocket.setblocking(0)
+
+
+
+		clientData = b""
+		targetHostData = b""
+		terminate = False
+		while not terminate and not terminateAll:
+			inputs = [self.__clientSocket, targetHostSocket]
+			outputs = []
+			
+			if len(clientData) > 0:
+				outputs.append(self.__clientSocket)
+				
+			if len(targetHostData) > 0:
+				outputs.append(targetHostSocket)
+			
+			try:
+				inputsReady, outputsReady, errorsReady = select.select(inputs, outputs, [], 1.0)
+			except Exception as e:
+				print(e)
+				break
+				
+			for inp in inputsReady:
+				if inp == self.__clientSocket:
+					try:
+						data = self.__clientSocket.recv(4096)
+					except Exception as e:
+						print(e)
+					
+					if data != None:
+						if len(data) > 0:
+							targetHostData += data
+						else:
+							terminate = True
+				elif inp == targetHostSocket:
+					try:
+						data = targetHostSocket.recv(4096)
+					except Exception as e:
+						print(e)
+						
+					if data != None:
+						if len(data) > 0:
+							clientData += data
+						else:
+							terminate = True
+						
+			for out in outputsReady:
+				if out == self.__clientSocket and len(clientData) > 0:
+					bytesWritten = self.__clientSocket.send(clientData)
+					if bytesWritten > 0:
+						clientData = clientData[bytesWritten:]
+				elif out == targetHostSocket and len(targetHostData) > 0:
+					bytesWritten = targetHostSocket.send(targetHostData)
+					if bytesWritten > 0:
+						targetHostData = targetHostData[bytesWritten:]
+		
+		self.__clientSocket.close()
+		targetHostSocket.close()
+		print("ClienThread terminating")
+
+if __name__ == '__main__':
+	if len(sys.argv) != 8:
+		print('Usage:\n\tpython SimpleTlsRedirector <host> <port> <server key path> <server cert path> <remote host> <remote port> <client cert path>')
+		print('Example:\n\tpython SimpleTlsRedirector localhost 8080 server_key.pem server_cert.pem www.google.com 80 client_cert.pem')
+		sys.exit(0)		
+	
+	localHost = sys.argv[1]
+	localPort = int(sys.argv[2])
+	skey = sys.argv[3]
+	scert = sys.argv[4]
+	targetHost = sys.argv[5]
+	targetPort = int(sys.argv[6])
+	tcert = sys.argv[7]
+
+	context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+	context.load_cert_chain(scert, skey)		
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serverSocket.bind((localHost, localPort))
+	serverSocket.listen(5)
+	serverSocket = context.wrap_socket(serverSocket, server_side=True)
+
+	print("Waiting for client...")
+	while True:
+		try:
+			clientSocket, address = serverSocket.accept()
+		except KeyboardInterrupt:
+			print("\nTerminating...")
+			terminateAll = True
+			break
+		ClientThread(clientSocket, targetHost, targetPort, tcert).start()
+		
+	serverSocket.close()
